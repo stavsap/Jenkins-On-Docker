76e42f84371900520a0d35da52f67b2ffe29e2af
Stav Sapir <stavsap@gmail.com>
2023-09-30 13:24:37 +0300

    Create jenkins-docker-clean-all.sh

---
+++ jenkins-docker-clean-all.sh
@@ -0,0 +1,12 @@
+docker rm -f jenkins-blueocean
+docker rm -f alpine-socat
+docker rm -f jenkins-docker-registry
+
+sleep 15
+
+docker network rm jenkins
+docker volume rm jenkins-data
+docker volume rm jenkins-docker-certs
+docker volume rm jenkins-registry-data
+
+echo "All Complete"
