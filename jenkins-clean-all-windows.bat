c2f3457d4f023809ba703df2cf37af84a4d6ec4e
Stav Sapir <stavsap@gmail.com>
2024-03-21 22:52:25 +0200

    Update jenkins-clean-all-windows.bat

--- jenkins-clean-all-windows.bat
+++ jenkins-clean-all-windows.bat
@@ -4,11 +4,11 @@ docker rm -f jenkins-blueocean
 docker rm -f alpine-socat
 docker rm -f jenkins-docker-registry
 
-sleep 15
+timeout 15 /nobreak
 
 docker network rm jenkins
 docker volume rm jenkins-data
 docker volume rm jenkins-docker-certs
 docker volume rm jenkins-registry-data
 
-echo "All Complete"
+echo Full Clean Complete
