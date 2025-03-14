d239670d9b6f38b0b8147cddf43c2df6f472373b
Stav Sapir <stavsap@gmail.com>
2025-03-14 09:38:40 +0200

    Update jenkins-docker-update-linux.sh

--- jenkins-docker-update-linux.sh
+++ jenkins-docker-update-linux.sh
@@ -3,12 +3,12 @@ cd Jenkins-On-Docker
 
 docker rm -f jenkins
 
+echo "Sleeping 5 sec."
 sleep 5
 
 docker rmi myjenkins:1
 
 docker build -t myjenkins:1 .
-docker network create jenkins
 
 echo "Running myjenkins:1..."
 docker run --name jenkins -d --restart=always --network jenkins -p 8080:8080 -p 50000:50000 -v jenkins-data:/var/jenkins_home -v jenkins-docker-certs:/certs/client:ro -e DOCKER_HOST=tcp://docker:2376 -e DOCKER_CERT_PATH=/certs/client -e DOCKER_TLS_VERIFY=1 myjenkins:1
